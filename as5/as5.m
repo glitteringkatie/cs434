0cad1e3c535211f7dd2db7ce7004606a80162e01
hugheska <hugheska@onid.orst.edu>
2015-05-28 18:17:06 -0700

    We finished as5

--- as5/as5.m
+++ as5/as5.m
@@ -7,26 +7,73 @@ function main = as5()
     X = fileMatrix(:,1:(end-1));
     %Get results
     Y = fileMatrix(:,end);
-
+    
+    Xp = PCA(X, 0.8);
+    
     %Number of training data entries (e.g. 1400)
     numDataEntries = size(X,1);
     for k = [2 4 6 8]
-        [best_centers, best_labels] = kmeans(X, k);
-        best_sse = getSSE(X, best_centers, best_labels);
-        for i = 1:(10 - 1)
-            [centers, labels] = kmeans(X, k);
-            sse = getSSE(X, best_centers, best_labels);
-            if (sse < best_sse)
-                best_sse = sse;
-                best_centers = centers;
-                best_labels = labels;
-            end
-        end
-        best_labels
-        best_centers
-        best_sse
+        [C, p] = evaluate(X, Y, k);
+        fprintf('Original Data Confusion Matrix for k = %d (purity = %f)\n', k, p);
+        disp(C);
+        [C, p] = evaluate(Xp, Y, k);
+        fprintf('Reduced Dimensional Data Confusion Matrix for k = %d (purity = %f)\n', k, p);
+        disp(C);
+        fprintf('----------------------------------------------\n\n');
     end
     main = 0;
+end
+
+function Xp = PCA(X, variance)
+    sigma = cov(X);
+    [vectors, values] = eig(sigma);
+    values = sum(values, 2);
+    [values, inds] = sort(values, 'descend');
+    vectors = vectors(inds, :);
+    
+    dp = 1;
+    for i = 1:size(values, 1)
+        if (sum(values(1:i)) / sum(values) > variance)
+            break
+        end
+        dp = i;
+    end
+    W = vectors(1:dp, :);
+    Xp = X * transpose(W);
+end
+
+function [C, p] = evaluate(X, Y, k)
+    [best_centers, best_labels] = kmeans(X, k);
+    best_sse = getSSE(X, best_centers, best_labels);
+    for i = 1:(10 - 1)
+        [centers, labels] = kmeans(X, k);
+        sse = getSSE(X, best_centers, best_labels);
+        if (sse < best_sse)
+            best_sse = sse;
+            best_centers = centers;
+            best_labels = labels;
+        end
+    end
+    C = confusion(Y, best_labels, k);
+    p = purity(C, Y, labels);
+end
+
+function p = purity(C, Y, labels)
+    [dummy, majorities] = max(C, [], 2);
+    majorities = majorities - 1;
+    Yp = zeros(size(Y));
+    for i = 1:size(labels, 1)
+        Yp(i) = majorities(labels(i));
+    end
+    p = sum(Yp == Y) / size(Y, 1);
+end
+
+function C = confusion(Y, labels, k)
+    C = zeros(k, 2);
+    
+    for i = 1:size(Y, 1)
+        C(labels(i), Y(i) + 1) = C(labels(i), Y(i) + 1) + 1;    
+    end
 end
 
 function sse = getSSE(X, centers, labels)
