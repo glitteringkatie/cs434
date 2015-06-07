f03b05579a41145c119eccc6736b96a5c2fbf104
Jasper LaFortune <--global>
2015-06-06 20:17:14 -0700

    added reformatter

--- final/classifier.py
+++ final/classifier.py
@@ -1,8 +1,6 @@
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.datasets import fetch_20newsgroups
-categories = ['alt.atheism', 'soc.religion.christian', 'comp.graphics', 'sci.med']
-twenty_train = fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=42)
-print type(twenty_train.data[0])
+
+
 
 # count_vect = CountVectorizer()
 # X_train_counts = count_vect.fit_transform(twenty_train.data)
---
+++ final/reformat.py
@@ -0,0 +1,17 @@
+# reformat lyrics as list
+import os
+
+def get_lyrics(folder):
+	
+	songs = []
+	for title in os.listdir(folder):
+		with open(folder + title) as songfile:
+			songs.append(songfile.read())
+
+	return songs
+
+folder = 'country_songs/'
+print get_lyrics(folder)
+
+
+
